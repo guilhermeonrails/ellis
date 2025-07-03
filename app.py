d04ecf09cee6e0c16769a9b0f209a67546610b5d
AGoretti <andremotta96@gmail.com>
2025-07-03 19:18:56 -0300

    Teste Integracao

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="API de Gestão Escolar", 
     description="""
-        Esta API fornece endpoints para gerenciar alunos, cursos e turmas, em uma instituição de ensino.  
-        
-        Permite realizar diferentes operações em cada uma dessas entidades.
+        Essa é uma API teste Com deploy continuo na gcloud por André Goretti
     """, 
     version="1.0.0",
 )
